7b6ca0b84da6b8c55a56ad2ac82dab89ea21e2a2
Romario <hallstar01@gmail.com>
2020-05-25 22:29:46 -0500

    (Updated) add forgot and reset routing

--- src/app/auth/routes/index.js
+++ src/app/auth/routes/index.js
@@ -1,9 +1,19 @@
-import { Login } from '../components'
+import { Login, Forgot, Reset } from '../components'
 
 export default [
     {
         path: '/login',
         component: Login,
         name: 'login',
+    },
+    {
+        path: '/forgot',
+        component: Forgot,
+        name: 'forgot',
+    },
+    {
+        path: '/reset/:token',
+        component: Reset,
+        name: 'reset',
     }
 ]
