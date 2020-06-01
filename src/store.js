f0360181fda58d0f19a4a7c178626413028ce02e
Romario <hallstar01@gmail.com>
2020-06-01 17:16:04 -0500

    Add settings vuex module with ability to fetch banks, currencies and brokers

--- src/store.js
+++ src/store.js
@@ -3,24 +3,26 @@ import Vuex from 'vuex'
 
 import auth from './app/auth/vuex'
 import offers from './app/offers/vuex'
+import settings from './app/settings/vuex'
 
 Vue.use(Vuex)
 
 const mutations = {
-    set (state, [variable, value]) {
+    set(state, [variable, value]) {
         state[variable] = value
     },
 }
 
 const state = {
-    sidebarShow: true
+    sidebarShow: true,
 }
 
 export default new Vuex.Store({
     modules: {
         auth,
-        offers
+        offers,
+        settings,
     },
     mutations,
-    state
+    state,
 })
