da12aa9282bfb73a9a55edae965c0eeed0e8f306
Romario <hallstar01@gmail.com>
2020-06-01 17:09:28 -0500

    Add antd and check for local storage of auth token then hydrate store acordingly

--- src/main.js
+++ src/main.js
@@ -4,10 +4,29 @@ import router from './router'
 import localforage from 'localforage'
 import store from './store'
 import { iconsSet as icons } from './assets/icons.js'
+import Antd from 'ant-design-vue'
 import 'core-js/stable'
 
 localforage.config({
   driver: localforage.LOCALSTORAGE,
+})
+Vue.use(Antd);
+
+store.dispatch('auth/setToken')
+  .then(() => {
+    store.dispatch('auth/fetchUser').catch(() => {
+        // These complicate the development process by clearing state with hot reload etc so disable them locally
+        if (process.env.NODE_ENV !== 'development') {
+            store.dispatch('auth/clearAuth').catch(() => {})
+            router.replace({ name: 'login' }).catch(() => {})
+        }
+    })
+})
+.catch(() => {
+    // These complicate the development process by clearing state with hot reload etc so disable them locally
+    if (process.env.NODE_ENV !== 'development') {
+        store.dispatch('auth/clearAuth').catch(() => {})
+    }
 })
 
 Vue.config.productionTip = false
