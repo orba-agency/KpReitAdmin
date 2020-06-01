41b4320745e448b5a94a4595d4f99e9c13aa1b28
Romario <hallstar01@gmail.com>
2020-06-01 17:18:29 -0500

    Add http interceptors to refresh token and remake request on receiving a 401 status error

--- src/repository.js
+++ src/repository.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {removeWrappedQuotesFromString} from './helpers'
+import { removeWrappedQuotesFromString } from './helpers'
 import localforage from 'localforage'
 import store from './store'
 import router from './router'
@@ -14,79 +14,80 @@ if (process.env.NODE_ENV === 'development') {
 const baseVersionedUrl = `${baseDomain}/api/v1/admin`
 
 let instance = axios.create({
-    baseURL: baseVersionedUrl
+    baseURL: baseVersionedUrl,
 })
 
-let isRefreshing = false;
-let refreshSubscribers = [];
+let isRefreshing = false
+let refreshSubscribers = []
 
 function subscribeTokenRefresh(cb) {
-   refreshSubscribers.push(cb);
+    refreshSubscribers.push(cb)
 }
 
 function onRrefreshed(token) {
-   refreshSubscribers.map((cb) => cb(token));
+    refreshSubscribers.map((cb) => cb(token))
 }
 
-instance.interceptors.response.use((response) => {
-    return response;
-}, (error) => {
-    // console.log('TCL: err', err);
+instance.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        // console.log('TCL: err', err);
 
-    if (!error.response) {
-        return Promise.reject(error);
-    }
+        if (!error.response) {
+            return Promise.reject(error)
+        }
 
-    const {
-        response: { status },
-    } = error;
+        const {
+            response: { status },
+        } = error
 
-    const originalRequest = error.config;
+        const originalRequest = error.config
 
-    if (
-        (originalRequest.url.indexOf('login') > -1 && status === 401) ||
-        (originalRequest.url.indexOf('logout') > -1 && status === 401)
-    ) {
-        return Promise.reject({ error: { message: 'Please try logging in again' } });
-    }
+        if (
+            (originalRequest.url.indexOf('login') > -1 && status === 401) ||
+            (originalRequest.url.indexOf('logout') > -1 && status === 401)
+        ) {
+            return Promise.reject({ error: { message: 'Please try logging in again' } })
+        }
 
-    // // Todo: change status to 498
-    console.log('TCL: err', status);
-    if (status === 401) {
-        if (!isRefreshing && localforage.getItem('auth_token')) {
-        console.log('TCL: err', status);
-        isRefreshing = true;
+        // // Todo: change status to 498
+        console.log('TCL: err', status)
+        if (status === 401) {
+            if (!isRefreshing && localforage.getItem('auth_token')) {
+                console.log('TCL: err', status)
+                isRefreshing = true
 
-        store.dispatch('auth/refreshToken').then((newToken) => {
-            if(newToken) {
-                isRefreshing = false;
-                onRrefreshed(newToken);
-                refreshSubscribers = [];
-            } else {
-                console.log(router.currentRoute);
-                store.dispatch('auth/clearAuth').then(() => {
-                    router.replace({ name: 'login' })
+                store.dispatch('auth/refreshToken').then((newToken) => {
+                    if (newToken) {
+                        isRefreshing = false
+                        onRrefreshed(newToken)
+                        refreshSubscribers = []
+                    } else {
+                        console.log(router.currentRoute)
+                        store.dispatch('auth/clearAuth').then(() => {
+                            router.replace({ name: 'login' })
+                        })
+                    }
                 })
+            } else {
+                return Promise.reject(error)
             }
-        })
-            
-    } else {
-        return Promise.reject(error);
+
+            const retryOrigReq = new Promise((resolve) => {
+                subscribeTokenRefresh((token) => {
+                    // replace the expired token and retry
+                    originalRequest.headers.Authorization = `Bearer ${removeWrappedQuotesFromString(token)}`
+                    resolve(instance(originalRequest))
+                })
+            })
+
+            return retryOrigReq
+        } else {
+            return Promise.reject(error)
+        }
     }
-
-    const retryOrigReq = new Promise((resolve) => {
-        subscribeTokenRefresh((token) => {
-            // replace the expired token and retry
-            originalRequest.headers.Authorization = `Bearer ${removeWrappedQuotesFromString(token)}`;
-            resolve(instance(originalRequest));
-        });
-    });
-
-    return retryOrigReq;
-
-    } else {
-        return Promise.reject(error);
-    }
-});
+)
 
 export default instance
