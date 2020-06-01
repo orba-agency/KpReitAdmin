2f63f997fd8416ac6af34278e767c82cb3799797
Romario <hallstar01@gmail.com>
2020-06-01 17:17:41 -0500

    Check local storage for token and hydrate store if exists

--- src/main.js
+++ src/main.js
@@ -8,35 +8,36 @@ import Antd from 'ant-design-vue'
 import 'core-js/stable'
 
 localforage.config({
-  driver: localforage.LOCALSTORAGE,
+    driver: localforage.LOCALSTORAGE,
 })
-Vue.use(Antd);
+Vue.use(Antd)
 
-store.dispatch('auth/setToken')
-  .then(() => {
-    store.dispatch('auth/fetchUser').catch(() => {
+store
+    .dispatch('auth/setToken')
+    .then(() => {
+        store.dispatch('auth/fetchUser').catch(() => {
+            // These complicate the development process by clearing state with hot reload etc so disable them locally
+            if (process.env.NODE_ENV !== 'development') {
+                store.dispatch('auth/clearAuth').catch(() => {})
+                router.replace({ name: 'login' }).catch(() => {})
+            }
+        })
+    })
+    .catch(() => {
         // These complicate the development process by clearing state with hot reload etc so disable them locally
         if (process.env.NODE_ENV !== 'development') {
             store.dispatch('auth/clearAuth').catch(() => {})
-            router.replace({ name: 'login' }).catch(() => {})
         }
     })
-})
-.catch(() => {
-    // These complicate the development process by clearing state with hot reload etc so disable them locally
-    if (process.env.NODE_ENV !== 'development') {
-        store.dispatch('auth/clearAuth').catch(() => {})
-    }
-})
 
 Vue.config.productionTip = false
 
-import CoreuiVue from '@coreui/vue';
-Vue.use(CoreuiVue);
+import CoreuiVue from '@coreui/vue'
+Vue.use(CoreuiVue)
 
 new Vue({
-  router,
-  store,
-  icons,
-  render: h => h(App),
+    router,
+    store,
+    icons,
+    render: (h) => h(App),
 }).$mount('#app')
