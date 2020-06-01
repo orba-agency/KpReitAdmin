3247fbc86c6edba01a3683a4f9384db8319e9e08
Romario <hallstar01@gmail.com>
2020-06-01 17:22:18 -0500

    Add helper for uploading

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,7 @@ import { isEmpty } from 'lodash'
 
 import Repository from '../repository'
 
-export const setHttpToken = token => {
+export const setHttpToken = (token) => {
     console.log(token)
     if (isEmpty(token)) {
         Repository.defaults.headers.common['Authorization'] = null
@@ -13,5 +13,22 @@ export const setHttpToken = token => {
 
 export const removeWrappedQuotesFromString = (str) => {
     // console.log('removeWrappedQuotesFromString', str && typeof str === 'string', str);
-    return str && typeof str === 'string' ? str.replace(/^"(.*)"$/, '$1') : str;
-};
+    return str && typeof str === 'string' ? str.replace(/^"(.*)"$/, '$1') : str
+}
+
+export const upload = async (file) => {
+    const formData = new FormData()
+    formData.append('file', file)
+
+    const config = {
+        headers: { 'content-type': 'multipart/form-data' },
+    }
+
+    try {
+        let result = await Repository.post('/uploads', formData)
+        return result.data
+    } catch (err) {
+        console.log(err)
+        return
+    }
+}
