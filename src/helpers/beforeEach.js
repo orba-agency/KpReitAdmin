6878c5e4d3091638799b588da1999ee9d255aadc
Romario <hallstar01@gmail.com>
2020-06-07 22:05:09 -0500

    Redirect to dashboard if user does not have permission for route

--- src/helpers/beforeEach.js
+++ src/helpers/beforeEach.js
@@ -1,10 +1,19 @@
 import store from '../store'
 import localforage from 'localforage'
+import { isEmpty } from 'lodash'
 
 const beforeEach = async (to, from, next) => {
     store
         .dispatch('auth/checkTokenExists')
         .then(() => {
+            if (
+                !isEmpty(to.meta.permission) &&
+                store.state.auth.user.permissions.findIndex((permission) => permission === to.meta.permission) === -1
+            ) {
+                next({ name: 'dashboard' })
+                return
+            }
+
             next()
         })
         .catch((error) => {
