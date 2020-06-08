32617aebbc165a8092767c256bbb9e11c7288033
hallstar <hallstar01@gmail.com>
2020-06-08 14:12:15 -0500

    Add actions to fetch, create, update and delete permissions

--- src/app/permissions/vuex/actions.js
+++ src/app/permissions/vuex/actions.js
@@ -6,3 +6,44 @@ export const fetchAll = ({ commit }, payload) => {
         return response.data
     })
 }
+
+export const fetchPermission = ({ commit }, { payload, context }) => {
+    return Repository.get(`/permissions/${payload.id}`)
+        .then((response) => {
+            context.permission = response.data
+        })
+        .catch((error) => {})
+}
+
+export const updatePermission = ({ commit }, { payload, context }) => {
+    context.errors = []
+    context.message = null
+
+    return Repository.put(`/permissions/${payload.id}`, payload.permission)
+        .then((response) => {})
+        .catch((error) => {
+            context.errors = error.response.data.errors || []
+            context.message = error.response.data.message
+        })
+}
+
+export const deleteRecord = ({ commit }, { payload, context }) => {
+    context.error = null
+    return Repository.delete(`/permissions/${payload.id}`)
+        .then(() => {})
+        .catch((error) => {
+            context.error = error.response.data.message
+        })
+}
+
+export const createPermission = ({ commit }, { payload, context }) => {
+    context.errors = []
+    context.message = null
+
+    return Repository.post('/permissions', payload)
+        .then((response) => {})
+        .catch((error) => {
+            context.errors = error.response.data.errors || []
+            context.message = error.response.data.message
+        })
+}
