1f2a03a6e4816fc47d857ac40f16000bc65ab028
hallstar <hallstar01@gmail.com>
2020-06-09 01:46:23 -0500

    Add user routes

--- src/app/users/routes/index.js
+++ src/app/users/routes/index.js
@@ -1,12 +1,40 @@
-import { Users } from '../components'
+import { Users, UserNew, UserView, UserEdit } from '../components'
 
 export default [
     {
         path: '/users',
         component: Users,
         name: 'users',
-        // meta: {
-        //     needsAuth: true,
-        // },
-    }
+        meta: {
+            isAuthenticated: true,
+            permission: 'user_access',
+        },
+    },
+    {
+        path: '/user-new',
+        component: UserNew,
+        name: 'user-new',
+        meta: {
+            isAuthenticated: true,
+            permission: 'user_create',
+        },
+    },
+    {
+        path: '/users/:id/edit',
+        component: UserEdit,
+        name: 'user-edit',
+        meta: {
+            isAuthenticated: true,
+            permission: 'user_edit',
+        },
+    },
+    {
+        path: '/users/:id',
+        component: UserView,
+        name: 'user-view',
+        meta: {
+            isAuthenticated: true,
+            permission: 'user_show',
+        },
+    },
 ]
