0f97c8b42d193e035b2ad438269d81185cdf3cfb
hallstar <hallstar01@gmail.com>
2020-06-08 23:17:54 -0500

    Add currency view component

---
+++ src/app/currencies/components/CurrencyView.vue
@@ -0,0 +1,48 @@
+<template>
+    <a-descriptions title="Currency Info">
+        <a-descriptions-item label="Name" span="3">
+            {{ currency.name }}
+        </a-descriptions-item>
+        <a-descriptions-item label="Code" span="3">
+            {{ currency.code }}
+        </a-descriptions-item>
+        <a-descriptions-item label="Symbol" span="3">
+            {{ currency.symbol }}
+        </a-descriptions-item>
+        <a-descriptions-item label="Precision" span="3">
+            {{ currency.precision }}
+        </a-descriptions-item>
+        <a-descriptions-item label="Template" span="3">
+            {{ currency.template }}
+        </a-descriptions-item>
+    </a-descriptions>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+    data() {
+        return {
+            currency: {},
+            state: {
+                loaded: false,
+            },
+        }
+    },
+    created() {
+        this.fetchCurrency({
+            payload: {
+                id: this.$route.params.id,
+            },
+            context: this,
+        }).then(() => {
+            this.state.loaded = true
+        })
+    },
+    methods: {
+        ...mapActions({
+            fetchCurrency: 'currencies/fetchCurrency',
+        }),
+    },
+}
+</script>
