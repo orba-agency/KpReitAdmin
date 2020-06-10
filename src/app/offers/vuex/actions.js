85cd7f29d0c97ab0e85bd273bc6b868d798eef56
hallstar <hallstar01@gmail.com>
2020-06-10 01:06:34 -0500

    Add gallery views and uploads actions

--- src/app/offers/vuex/actions.js
+++ src/app/offers/vuex/actions.js
@@ -25,3 +25,36 @@ export const createOffer = ({ commit }, { payload, context }) => {
             context.message = error.response.data.message
         })
 }
+
+export const updateOffer = ({ commit }, { payload, context }) => {
+    context.errors = []
+    context.message = null
+
+    return Repository.put(`/offers/${payload.id}`, payload.form)
+        .then((response) => {})
+        .catch((error) => {
+            context.errors = error.response.data.errors || []
+            context.message = error.response.data.message
+        })
+}
+
+export const updateGallery = ({ commit }, { payload, context }) => {
+    context.error = null
+
+    return Repository.put(`/offers/${payload.id}/galleries/${payload.upload_id}`, {})
+        .then((response) => {
+            return response.data
+        })
+        .catch((error) => {
+            context.error = error.response.data.message
+        })
+}
+
+export const deleteGallery = ({ commit }, { payload, context }) => {
+    context.error = null
+    return Repository.delete(`/offers/${payload.id}/galleries/${payload.gallery_id}`)
+        .then(() => {})
+        .catch((error) => {
+            context.error = error.response.data.message
+        })
+}
