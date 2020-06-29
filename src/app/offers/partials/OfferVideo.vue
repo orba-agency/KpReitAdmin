4c1cbb712d6c7e53290b385e27bcd9437eca44a6
hallstar <hallstar01@gmail.com>
2020-06-28 23:13:31 -0500

    Add new video view component for viewing of offers

---
+++ src/app/offers/partials/OfferVideo.vue
@@ -0,0 +1,31 @@
+<template>
+    <a-table :columns="columns" :data-source="videos">
+        <a slot="name" slot-scope="text">{{ text }}</a>
+    </a-table>
+</template>
+
+<script>
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'URL',
+        dataIndex: 'url',
+        key: 'url',
+    },
+]
+
+export default {
+    props: {
+        videos: Array,
+    },
+    data() {
+        return {
+            columns,
+        }
+    },
+}
+</script>
