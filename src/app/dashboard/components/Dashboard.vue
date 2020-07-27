2ca9c72ed78c2c7a127044e64873c3b71c0998cd
hallstar <hallstar01@gmail.com>
2020-07-27 15:38:52 -0500

    Add static widgets for dashboard

--- src/app/dashboard/components/Dashboard.vue
+++ src/app/dashboard/components/Dashboard.vue
@@ -1,13 +1,51 @@
 <template>
     <div>
-        Dashboard
+        <a-row :gutter="16">
+            <a-col :span="8">
+                <a-statistic title="Clients" :value="1128" style="margin-right: 50px;">
+                    <template #suffix>
+                        <a-icon type="usergroup-add" />
+                    </template>
+                </a-statistic>
+            </a-col>
+            <a-col :span="8">
+                <a-statistic title="Applications" :value="1128" style="margin-right: 50px;">
+                    <template #suffix>
+                        <a-icon type="schedule" />
+                    </template>
+                </a-statistic>
+            </a-col>
+            <a-col :span="8">
+                <a-statistic title="Offers" :value="1128" style="margin-right: 50px;">
+                    <template #suffix>
+                        <a-icon type="bulb" />
+                    </template>
+                </a-statistic>
+            </a-col>
+        </a-row>
+
+        <a-row :gutter="16">
+            <a-col :span="12" style="padding: 30px;">
+                <h4 style="text-align: center;">Application Status</h4>
+                Draft
+                <a-progress :percent="30" />
+                Pending
+                <a-progress :percent="50" status="active" />
+                Rejected
+                <a-progress :percent="70" status="exception" />
+                Approved
+                <a-progress :percent="100" />
+            </a-col>
+            <a-col :span="12" style="padding: 30px; text-align: center;">
+                <h4>Offer Status</h4>
+                <a-progress type="circle" :percent="75" />
+                <a-progress type="circle" :percent="70" status="exception" />
+                <a-progress type="circle" :percent="100" />
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script>
-
-export default {
-    
-}
-
+export default {}
 </script>
