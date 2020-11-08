3b0fb8a1c3737a8946e1df24c98bf4c03f831df7
hallstar <hallstar01@gmail.com>
2020-11-07 21:24:50 -0500

    Allow admin to create client

--- src/app/clients/components/ClientNew.vue
+++ src/app/clients/components/ClientNew.vue
@@ -11,11 +11,39 @@
             </a-row>
             <a-form-model ref="ruleForm" :model="form" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol">
                 <a-form-model-item
+                    label="Account Type"
+                    prop="type"
+                    :validate-status="errors.type ? 'error' : ''"
+                    :help="errors.type ? errors.type[0] : null"
+                >
+                    <a-select v-model="form.type" placeholder="Please select a type">
+                        <a-select-option value="Individual">
+                            Individual
+                        </a-select-option>
+                        <a-select-option value="Corporation">
+                            Corporation
+                        </a-select-option>
+                    </a-select>
+                </a-form-model-item>
+
+                <a-form-model-item
+                    ref="corporation"
+                    label="Corporation"
+                    prop="corporation"
+                    v-if="form.type === 'Corporation'"
+                    :validate-status="errors.corporation ? 'error' : ''"
+                    :help="errors.corporation ? errors.corporation[0] : null"
+                >
+                    <a-input v-model="form.corporation" />
+                </a-form-model-item>
+
+                <a-form-model-item
                     ref="first_name"
                     label="First Name"
                     prop="first_name"
+                    v-if="form.type === 'Individual'"
                     :validate-status="errors.first_name ? 'error' : ''"
-                    :help="errors.title ? errors.first_name[0] : null"
+                    :help="errors.first_name ? errors.first_name[0] : null"
                 >
                     <a-input v-model="form.first_name" />
                 </a-form-model-item>
@@ -24,8 +52,9 @@
                     ref="last_name"
                     label="Last Name"
                     prop="last_name"
+                    v-if="form.type === 'Individual'"
                     :validate-status="errors.last_name ? 'error' : ''"
-                    :help="errors.title ? errors.last_name[0] : null"
+                    :help="errors.last_name ? errors.last_name[0] : null"
                 >
                     <a-input v-model="form.last_name" />
                 </a-form-model-item>
@@ -35,7 +64,7 @@
                     label="Email Address"
                     prop="email"
                     :validate-status="errors.email ? 'error' : ''"
-                    :help="errors.title ? errors.email[0] : null"
+                    :help="errors.email ? errors.email[0] : null"
                 >
                     <a-input v-model="form.email" />
                 </a-form-model-item>
@@ -45,29 +74,9 @@
                     label="Password"
                     prop="password"
                     :validate-status="errors.password ? 'error' : ''"
-                    :help="errors.title ? errors.password[0] : null"
+                    :help="errors.password ? errors.password[0] : null"
                 >
-                    <a-input v-model="form.password" />
-                </a-form-model-item>
-
-                <a-form-model-item
-                    ref="roles"
-                    label="Roles"
-                    prop="roles"
-                    :validate-status="errors.roles ? 'error' : ''"
-                    :help="errors.roles ? errors.roles[0] : null"
-                >
-                    <a-select
-                        mode="multiple"
-                        :default-value="form.roles"
-                        style="width: 100%;"
-                        placeholder="Please select roles"
-                        @change="handleChange"
-                    >
-                        <a-select-option v-for="val in roles" :key="val.id">
-                            {{ val.title }}
-                        </a-select-option>
-                    </a-select>
+                    <a-input-password v-model="form.password" />
                 </a-form-model-item>
 
                 <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
@@ -97,32 +106,29 @@ export default {
                 processing: false,
             },
             form: {
-                first_name: null,
-                last_name: null,
-                email: null,
-                password: null,
-                roles: [],
+                type: 'Individual',
+                corporation: null,
+                first_name: '',
+                last_name: '',
+                email: '',
+                password: '',
             },
             rules: {
+                corporation: [{ required: true, message: 'Please add corporation', trigger: 'blur' }],
                 first_name: [{ required: true, message: 'Please add first name', trigger: 'blur' }],
                 last_name: [{ required: true, message: 'Please add last name', trigger: 'blur' }],
                 email: [{ required: true, message: 'Please add email', trigger: 'blur' }],
                 password: [{ required: true, message: 'Please add password', trigger: 'blur' }],
-                roles: [{ required: true, message: 'Please select role', trigger: 'change' }],
             },
         }
     },
-    created() {
-        this.fetchAll({ all: true }).then((resp) => {
-            this.roles = resp
-        })
-    },
+    created() {},
     methods: {
         ...mapActions({
-            createUser: 'users/createUser',
-            fetchAll: 'roles/fetchAll',
+            createUser: 'clients/createUser',
         }),
         onSubmit() {
+            this.errors = []
             this.$refs.ruleForm.validate((valid) => {
                 if (valid) {
                     this.submitButton.processing = true
@@ -132,14 +138,14 @@ export default {
                         payload: this.form,
                         context: this,
                     }).then((response) => {
-                        console.log(response)
                         this.submitButton.processing = false
                         this.submitButton.name = 'Create'
 
-                        if (this.errors.root) {
+                        console.log(response.id)
+                        if (this.errors.length) {
                             return
                         } else {
-                            this.$router.replace({ name: 'users' })
+                            this.$router.replace({ name: 'clients' })
                         }
                     })
                 } else {
